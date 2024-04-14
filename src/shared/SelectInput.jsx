19393be53ae4b6790f654f29334784b251e4cf44
Kirill <kirillmas222@gmail.com>
2024-04-14 04:44:42 +0400

    foods

--- src/shared/SelectInput.jsx
+++ src/shared/SelectInput.jsx
@@ -14,8 +14,13 @@ export default function SelectInput(field) {
         multiple
         options={field.foodsList}
         getOptionLabel={(option) => option.label}
-        isOptionEqualToValue={(option, value) => option.id === value.id}
-        onChange={(event, value) => field.onChange(value)}
+        isOptionEqualToValue={(option, value) =>
+          option.id === (value ? value.id : null)
+        }
+        onChange={(event, value) => {
+          console.log('Value:', value) // Проверяем значение value
+          field.onChange(value ? value.map((val) => val.id) : null) // Получаем массив id и передаем
+        }}
         filterSelectedOptions
         renderInput={(params) => <UserInput {...params} />}
       />
