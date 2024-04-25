d580b9db30ea8984d1e6df440bc89809b9f02e59
Kirill <kirillmas222@gmail.com>
2024-04-26 03:05:41 +0400

    def values

---
+++ src/defaultValues.js
@@ -0,0 +1,55 @@
+const users = [
+  {
+    id: 1,
+    username: 'Valera',
+    email: 'asd@mail.re',
+    birthdate: '12.12.2023',
+    favorite_food_ids: [1, 3],
+    photo_id: null,
+  },
+  {
+    id: 2,
+    username: 'Maksim',
+    email: 'maks@mail.re',
+    birthdate: '18.03.2013',
+    favorite_food_ids: [5],
+    photo_id: null,
+  },
+  {
+    id: 3,
+    username: 'Sasha',
+    email: 'veron@mail.re',
+    birthdate: '04.10.1998',
+    favorite_food_ids: [''],
+    photo_id: null,
+  },
+]
+
+const foodList = [
+  {
+    id: 1,
+    label: 'Морковка',
+  },
+  {
+    id: 2,
+    label: 'Свекла',
+  },
+  {
+    id: 3,
+    label: 'Капуста',
+  },
+  {
+    id: 4,
+    label: 'Редиска',
+  },
+  {
+    id: 5,
+    label: 'Баклажан',
+  },
+  {
+    id: 6,
+    label: 'Огурец',
+  },
+]
+
+export { users, foodList }
